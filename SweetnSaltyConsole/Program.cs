7c18ba7410604f68b1544fd7785863f09e5f4286
Gulomjon Saidov <gulomjon.saidov@revature.net>
2021-09-15 19:45:26 -0700

    Initialized SweetnSaltyConsole

---
+++ SweetnSaltyConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SweetnSaltyConsole
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome!");
+    }
+  }// End of class
+}//End of Namespace
